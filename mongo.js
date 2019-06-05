84bb57212a65117b4e2498cdb1a792e54d710ece
Kimi Heinonen <kimiheinonen@hotmail.com>
2019-06-05 13:19:24 +0300

    fixed

--- mongo.js
+++ mongo.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose")
 
-const password = "cg6TwxkhLLijoP"
-const url = 'mongodb+srv://paakayttaja:cg6TwxkhLLijoP@fstrainmongodb-3ve2c.mongodb.net/foodpickerdb'
 const url = process.env.MONGODB_URL
 
 mongoose.connect(url)
