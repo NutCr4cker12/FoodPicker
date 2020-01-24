6e633a4480fc0b987891a58fcbde992dbb2922c0
Kimi Heinonen <kimiheinonen@hotmail.com>
2020-01-24 20:47:21 +0200

    fixing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
 const app = require('./app');
 const hostname = app.get('host');
-const port = app.get('port');
-const server = app.listen(port, hostname);
+const port = app.get('port') || 3001;
+const server = app.listen(port)
+// const server = app.listen(port, hostname);
 
 process.on('unhandledRejection', (reason, p) =>
   console.error('Unhandled Rejection at: Promise ', p, reason)  
