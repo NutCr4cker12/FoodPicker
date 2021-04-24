056669af2082df8579871e799fd3adb5b0c028e5
Kimi Heinonen <kimiheinonen@hotmail.com>
2021-04-24 21:20:14 +0300

    autobuild

--- backend/src/services/pending-payments/pending-payments.hooks.js
+++ backend/src/services/pending-payments/pending-payments.hooks.js
@@ -4,20 +4,25 @@ const { allowOnlyRoles } = require('../restrict');
 const { Forbidden } = require('@feathersjs/errors');
 
 const restrictGetAfter = async hook => {
-    if (!hook.params.provider)
-        return;
-
-    const res = hook.result;
-    if (res && !res.isMarked && res.ids) {
-        for (let i = 0; i < res.ids.length; i++) {
-            await hook.app.service("v1/payments").patch(res.ids[i], { paid: true })
+    try {
+        if (!hook.params.provider)
+            return;
+    
+        const res = hook.result;
+        if (res && !res.isMarked && res.ids) {
+            for (let i = 0; i < res.ids.length; i++) {
+                await hook.app.service("v1/payments").patch(res.ids[i], { paid: true })
+            }
+    
+            await hook.app.service("v1/pending-payments").patch(res._id, { isMarked: true })
+            hook.result = null;
+        } else if (res && res.isMarked) {
+            hook.result = { alreadyMarked: true }
+        } else {
+            hook.result = null;
         }
-        await hook.app.service("v1/pending-payments").patch(res._id, { isMarked: true })
-        hook.result = null;
-    } else if (res && res.isMarked) {
-        hook.result = { alreadyMarked: true }
-    } else {
-        hook.result = null;
+    } catch (error) {
+        console.error(error)
     }
     return hook;
 }
@@ -29,42 +34,42 @@ const restrictPatch = async hook => {
 }
 
 module.exports = {
-  before: {
-    all: [],
-    find: [
-        authenticate('jwt'),
-		allowOnlyRoles("admin")
-    ],
-    get: [],
-    create: [
-        authenticate('jwt'),
-		allowOnlyRoles("admin")
-    ],
-    update: [Forbidden],
-    patch: [restrictPatch],
-    remove: [
-        authenticate('jwt'),
-		allowOnlyRoles("admin")
-    ],
-  },
+    before: {
+        all: [],
+        find: [
+            authenticate('jwt'),
+            allowOnlyRoles("admin")
+        ],
+        get: [],
+        create: [
+            authenticate('jwt'),
+            allowOnlyRoles("admin")
+        ],
+        update: [Forbidden],
+        patch: [restrictPatch],
+        remove: [
+            authenticate('jwt'),
+            allowOnlyRoles("admin")
+        ],
+    },
 
-  after: {
-    all: [],
-    find: [],
-    get: [restrictGetAfter],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [],
+        find: [],
+        get: [restrictGetAfter],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    }
 };
